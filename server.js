f81b8121666b205e95f3b2fb4e910819d47f7018
EmiRivero2003 <eriveronovo@byupathway.edu>
2026-05-21 20:52:18 -0300

    fix navigation routes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
-import { getAllProjects } from './src/models/projects.js';
-import { getAllCategories } from './src/models/categories.js';
+// import { getAllProjects } from './src/models/projects.js';
+// import { getAllCategories } from './src/models/categories.js';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -37,20 +37,23 @@ app.get('/', async (req, res) => {
 app.get('/organizations', async (req, res) => {
     const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
+    res.render('organizations', {
+        title,
+        organizations
+    });
 
-    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
-  const projects = await getAllProjects();
   const title = 'Service Projects';
+  const projects = [];
 
   res.render('projects', { title, projects });
 });
 
 app.get('/categories', async (req, res) => {
-  const categories = await getAllCategories();
   const title = 'Service Project Categories';
+  const categories = [];
 
   res.render('categories', { title, categories });
 });
